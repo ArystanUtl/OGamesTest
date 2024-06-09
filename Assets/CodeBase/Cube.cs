b9dd9f76f104862301250ecc051f57d183f57e46
EmptyObject1 <EmptyObject1@github.com>
2024-06-10 02:14:43 +0500

    Base cube.

--- Assets/CodeBase/Cube.cs
+++ Assets/CodeBase/Cube.cs
@@ -1,8 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-    
+    [SerializeField] private Renderer meshRenderer;
+
+
+    public void SetColor(Color color)
+    {
+        if (!meshRenderer)
+            return;
+        
+        if (meshRenderer.material)
+            meshRenderer.material.color = color;
+    }
 }
