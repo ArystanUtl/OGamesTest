849f79cadc387620c364e22d203497b13e86bbc8
EmptyObject1 <EmptyObject1@github.com>
2024-06-10 02:09:31 +0500

    Basic logic for cube.

---
+++ Assets/CodeBase/GlobalData/GameConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeBase.GlobalData
+{
+    public class GameConstants
+    {
+        public const float DELAY_BETWEEN_ATTACK = 1f;
+    }
+}
---
+++ Assets/CodeBase/Service/GameFabric.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Service
+{
+    public class GameFabric: MonoBehaviour
+    {
+        [SerializeField] private Cube cubePrefab;
+
+
+        public Cube CreateCube(Vector3 position)
+        {
+            var cube = Instantiate(cubePrefab, position, Quaternion.identity);
+            return cube;
+        }
+        
+        
+    }
+}
